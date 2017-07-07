9fe0e8d00d19f7b3490dbe24628e6ab51fb79f43
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-07 13:07:59 +0300

    added skill test

--- src/tests.py
+++ src/tests.py
@@ -13,6 +13,10 @@ class TestSkillMethods(unittest.TestCase):
         with self.assertRaises(InvalidInputError):
             skill = Skill(bad_input)
 
+    def test_skill_is_initialized_as_unfinished(self):
+        new_skill = Skill("learn agile")
+        self.assertFalse(new_skill.is_done)
+
 
 if __name__ == '__main__':
     unittest.main()
