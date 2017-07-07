a8b104d4bd8bccc869af148a1a35228e4133955f
Eric Nelson Elem <eric.elem@outlook.com>
2017-07-07 13:32:39 +0300

    Add comments

--- src/user.py
+++ src/user.py
@@ -1,6 +1,8 @@
-class user:
+class User:
+    # Initializes a user with the name attribute passed
     def __init__(self, name):
         self.name =name
-
+    
+    # Returns the name of a user
     def get_name(self):
         return self.name
