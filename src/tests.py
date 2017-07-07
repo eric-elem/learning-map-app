f21635a6e810fd415eb16fd69a6be2b175edc18a
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-07-07 14:58:21 +0300

    Add . gitignore file and Add unit tests for the User class

--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from src.skill import InvalidInputError, Skill
+from src.user import User
 
 
 class TestSkillMethods(unittest.TestCase):
@@ -16,6 +17,14 @@ class TestSkillMethods(unittest.TestCase):
     def test_skill_is_initialized_as_unfinished(self):
         new_skill = Skill("learn agile")
         self.assertFalse(new_skill.is_done)
+
+
+class TestUserMethods(unittest.TestCase):
+
+    def test_username(self):
+        a_user = User("Homer")
+        self.assertEqual('Homer', a_user.name, msg="Should have Homer as the username")
+
 
 
 if __name__ == '__main__':
