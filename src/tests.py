5c034f5a99984ad053bd5442d2ea88598aa1242b
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-07 12:16:31 +0300

    added skill class v1

---
+++ src/tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.skill import InvalidInputError, Skill
+
+
+class TestSkillMethods(unittest.TestCase):
+
+    def test_invalid_input(self):
+        bad_input = None
+        with self.assertRaises(InvalidInputError):
+            skill = Skill(bad_input)
+        bad_input = ""
+        with self.assertRaises(InvalidInputError):
+            skill = Skill(bad_input)
